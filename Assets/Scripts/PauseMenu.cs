c90f05c300226a49238c93d6f48beaf34e288647
Jasmine-Riley <jasminein98@gmail.com>
2026-04-12 01:10:21 +0900

    Jasmine
    Pausemenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
