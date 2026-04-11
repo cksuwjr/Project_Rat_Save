6b18ed36c9d513fb1457ba4a620f7bce1bbd35c8
cksuwjr <cksuwjr@naver.com>
2026-04-11 19:37:19 +0900

    weapon hold

---
+++ Assets/Scripts/Weapon/WeaponObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponObject : MonoBehaviour
+{
+    public WeaponType weaponType;
+    public Vector3 weaponEquipPos;
+    public Vector3 weaponEquipRot;
+    public Vector3 weaponEquipScale;
+
+    public bool isUse = false;
+}
